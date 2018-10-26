03072397ef9f7db51f8ecff171249a0363bebf32
github koro <koro1008.it@gmail.com>
2018-10-26 17:15:49 +0900

    広告追加（署名鍵？のせいでアプリストアにあげられず…）

--- GokonSeki/app/src/main/java/com/dotinstall/gokonseki/MainActivity.java
+++ GokonSeki/app/src/main/java/com/dotinstall/gokonseki/MainActivity.java
@@ -6,12 +6,22 @@ import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
 
-public class MainActivity extends AppCompatActivity {
+import com.google.android.gms.ads.AdRequest;
+import com.google.android.gms.ads.AdView;
+import com.google.android.gms.ads.MobileAds;
 
+public class MainActivity extends AppCompatActivity {
+    //広告用
+    private AdView mAdView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        //広告処理
+        MobileAds.initialize(this, "ca-app-pub-6298264304843789~9374084181");
+        mAdView = findViewById(R.id.adView);
+        AdRequest adRequest = new AdRequest.Builder().build();
+        mAdView.loadAd(adRequest);
 
         Button start = findViewById(R.id.start);
         start.setOnClickListener(new View.OnClickListener() {
